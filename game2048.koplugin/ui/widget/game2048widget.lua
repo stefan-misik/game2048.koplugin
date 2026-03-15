98cd2f664dc9f53b7c37c22acfcca83d9152a8f2
Stefan Misik <stefan-misik@users.noreply.github.com>
2026-03-15 13:32:01 +0100

    feat: allow "fast" redraws on game widget when theme is black and white

--- game2048.koplugin/ui/widget/game2048widget.lua
+++ game2048.koplugin/ui/widget/game2048widget.lua
@@ -66,6 +66,7 @@ local Game2048Widget = InputContainer:extend{
     _has_focus = false,
     _is_active = false,
     _hidden_tile_index = 0,
+    _is_fast_ui = false,
 }
 
 function Game2048Widget:init()
@@ -87,6 +88,9 @@ function Game2048Widget:init()
     end
     -- Adjust bold face (source: textwidget.lua)
     self.face, self.bold = Font:getAdjustedFace(self.face, self.bold)
+
+    -- Check the palette
+    self:_checkPalette()
 
     -- Register events
     self:_registerKeyEvents()
@@ -168,13 +172,25 @@ function Game2048Widget:setNumbers(numbers, animate_new_tile)
         else
             self._hidden_tile_index = 0
         end
-        UIManager:setDirty(self.show_parent or self, "ui", self.dimen)
+        UIManager:setDirty(self.show_parent or self, self._is_fast_ui and "fast" or "ui", self.dimen)
     end
 end
 
 function Game2048Widget:setPalette(palette)
     self.palette = palette
+    self:_checkPalette()
     UIManager:setDirty(self.show_parent or self, "ui", self.dimen)
+end
+
+function Game2048Widget:_checkPalette()
+    local is_fast_ui = true
+    for _, color in ipairs(self.palette) do
+        if color ~= Blitbuffer.COLOR_WHITE and color ~= Blitbuffer.COLOR_BLACK then
+            is_fast_ui = false
+            break
+        end
+    end
+    self._is_fast_ui = is_fast_ui
 end
 
 function Game2048Widget:_uncoverHiddenTile()
